b08cbc70629cd1d8482058a3a5bfd027921e9f29
morgane <morgane.ortiz133@gmail.com>
2025-02-18 09:48:49 +0100

    profile

---
+++ frontend/asset/js/profile.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.dropbtn').forEach(button => {
+    button.addEventListener('click', function() {
+        const dropdownContent = this.nextElementSibling;
+        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    });
+}); 
